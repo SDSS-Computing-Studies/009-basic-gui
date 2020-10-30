b574c39cb4564e632a51b23e701c8de555ec05b7
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-29 19:45:20 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,3 +7,19 @@ the contents of the first 2 entry boxes. It will
 find the product and display the answer in the
 3rd entry box
 """
+
+#importing the necessary commands for tkinter 
+import tkinter as tk 
+from tkinter import *
+from tkinter import ttk
+
+window = tk.TK()
+
+def reset():
+    currentsum=0
+    e1.insert(INSERT,str(currentsum))
+
+currentproduct = 0 
+
+
+window.mainloop()
